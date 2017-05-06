fab5701bbfcec2a5d4ca75211ddd1cfa6f4f9de8
turlygazhy <erasul_32@mail.ru>
2017-05-06 13:43:47 +0600

    add last sql

--- src/main/java/com/turlygazhy/connection_pool/ConnectionPool.java
+++ src/main/java/com/turlygazhy/connection_pool/ConnectionPool.java
@@ -19,7 +19,7 @@ public class ConnectionPool {
     private static final Logger logger = LoggerFactory.getLogger(ConnectionPool.class);
 
     private static final int CONNECTION_NUMBER = 10;
-    private static final String JDBC_URL = "jdbc:h2:~/saryarka_akimat12";
+    private static final String JDBC_URL = "jdbc:h2:~/saryarka_akimat15";
     private static final String DB_LOGIN = "user";
     private static final String DB_PASSWORD = "user";
 
